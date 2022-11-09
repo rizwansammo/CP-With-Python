9300f568c4bbcbf723c03e842481c0436e59def7
Zero Byte <mrizwan.sammo@gmail.com>
2022-11-09 11:20:43 +0600

    Matrix Operaton

---
+++ matrixAddition.py
@@ -0,0 +1,14 @@
+#Matrix Addition
+##ParrotSec OS
+###Zero Byte
+import numpy as np
+
+A = np.array([[1, 2], [3, 4]])
+
+B = np.array([[4, 5], [6, 7]])
+
+print(f"Matrix A: \n{A}")
+
+print(f"Matrix A: \n{A}")
+
+print(f"Addition of Matrix A and B: \n {np.add(A, B)}")
---
+++ matrixSubtraction.py
@@ -0,0 +1,13 @@
+#Matrix Subtraction
+##ParrotSec OS
+###Zero Byte
+import numpy as np
+A = np.array([[1, 2], [3, 4]])
+
+B = np.array([[4, 5], [6, 7]])
+
+print(f"Matrix A: \n{A}")
+
+print(f"Matrix A: \n{A}")
+
+print(f"Addition of Matrix A and B: \n {np.subtract(A, B)}")
