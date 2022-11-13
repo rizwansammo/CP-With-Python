ea5580adbbec4424cb8b9547d91485467b8b4058
Rizwan Sammo <mrizwan.sammo@gmail.com>
2022-11-13 20:51:07 +0600

    limit

--- possibleString.py
+++ possibleString.py
@@ -4,8 +4,11 @@ import random
 import math
 counter = 0
 char_list = ['a','e','i','o','u']
-#limit
+#Or
+#limit = math.factorial(len(char_list))
 while counter <= math.factorial(len(char_list)):
+#then
+#while counter <= limit:
     random.shuffle(char_list)
     print(''.join(char_list))
     counter+=1
