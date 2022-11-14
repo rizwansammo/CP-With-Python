9a4c8a1339c7059f5910305e1e74484fab1a3db4
Rizwan Sammo <mrizwan.sammo@gmail.com>
2022-11-14 23:30:45 +0600

    Test 2

--- sumOfWholeList.py
+++ sumOfWholeList.py
@@ -1,14 +1,14 @@
 
 def calculateSum(l):
     sum=0
-    for i in l: #parmeter must be a list
+    li = []
+    lenth = int(input("Lenth of the list: "))
+    while len(li) < lenth:
+        l = input("Enter your Item to the List: ")
+        li.append(l)
+    for i in li: #parmeter must be a list
         sum+=i
     return sum
 
 
-li = []
-lenth = int(input("Lenth of the list: "))
-while len(li) < lenth:
-    l = input("Enter your Item to the List: ")
-    li.append(l)
 print(calculateSum([1,2,3]))
