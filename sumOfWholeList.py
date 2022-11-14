f25e72c4e663587e353a883348ae745202402676
Rizwan Sammo <mrizwan.sammo@gmail.com>
2022-11-14 23:40:27 +0600

    No Parameter Function

--- sumOfWholeList.py
+++ sumOfWholeList.py
@@ -1,11 +1,13 @@
 
-def calculateSum(l):
+def calculateSum():
     sum=0
     li = []
     lenth = int(input("Lenth of the list: "))
     while len(li) < lenth:
-        l = input("Enter your Item to the List: ")
+        l = int(input("Enter your Item to the List: "))
         li.append(l)
     for i in li: #parmeter must be a list
         sum+=i
     print(f"Sum is {sum}")
+
+calculateSum()
